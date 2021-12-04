a3c1c702bb3bb1ab78cad930880d39c3f0448976
Thomas Niccolo Reyes <niccoreyes@gmail.com>
2021-12-04 17:05:46 +0800

    learning main stateful widget setup and keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _myApp createState() => _myApp();
+}
+
+class _myApp extends State<MyApp> {
+  String dateDue = "august";
+
+  void calculateDueDate() {
+    setState(() {
+      dateDue = "summer";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Calculator test'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('Insert due date'),
-              ),
-              TextField(),
-              DecoratedBox(
-                decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.secondary),
-                child: const Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text('test'),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Spacer(),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Insert due date'),
                 ),
-              ),
-            ],
+                TextField(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Due: $dateDue'),
+                ),
+                const Spacer()
+              ],
+            ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: calculateDueDate,
+          child: const Icon(Icons.check),
         ),
       ),
     );
