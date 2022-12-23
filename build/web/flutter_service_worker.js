e0b9e4fb3e666869f134b9f80d4b075b9efb59ef
Thomas Niccolo Reyes <niccoreyes@gmail.com>
2022-12-23 22:27:50 +0800

    updated Web build href

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0a8cef5e9e52de10d08f6a2afe773ebf",
-"/": "0a8cef5e9e52de10d08f6a2afe773ebf",
+"index.html": "88e41e5b18fdbc64be9dde78210b2d84",
+"/": "88e41e5b18fdbc64be9dde78210b2d84",
 "main.dart.js": "df76b01ee5e530f40bd3655a1165b73b",
 "manifest.json": "fff52e010ed8cf0950f8808ed5b1b60a",
 "version.json": "c66fffab4eb76a765a44f192005ee879"
